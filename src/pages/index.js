7c87a0a08201a99c14671a85e4a2a279115520a7
dannyyys <dannyyys@users.noreply.github.com>
2021-07-08 00:01:14 +0800

    refactor: :recycle: Update export format

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,15 @@
 import { Box } from "@chakra-ui/react";
+
 import Camera from "../components/Camera";
 import Voice from "../components/Voice";
 
-export default function Home() {
+const Index = () => {
   return (
     <Box>
       <Camera />
       <Voice />
     </Box>
   );
-}
+};
+
+export default Index;
