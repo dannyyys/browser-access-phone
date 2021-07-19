738d55a48feed16847e979a91ba0c6fedb7a8691
dannyyys <dannyyys@users.noreply.github.com>
2021-07-19 23:20:18 +0800

    style: :lipstick: Update layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,21 @@
-import { Box } from "@chakra-ui/react";
+import Link from "next/link";
+import { Box, IconButton, Heading, VStack } from "@chakra-ui/react";
+import { AiFillCamera } from "react-icons/ai";
 
 import Camera from "../components/Camera";
 import Voice from "../components/Voice";
 
 const Index = () => {
   return (
-    <Box>
+    <Box align="center">
       <Camera />
       <Voice />
+      <VStack>
+        <Heading>Camera 2</Heading>
+        <Link href="/Camera2">
+          <IconButton icon={<AiFillCamera />} />
+        </Link>
+      </VStack>
     </Box>
   );
 };
